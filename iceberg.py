3bf4c16d69771c5810062a2447960bc00f45304b
Deepak Pradeep <deepakpradeepbe@gmail.com>
2025-04-07 10:14:52 +0530

    Update iceberg.py

--- iceberg.py
+++ iceberg.py
@@ -3,8 +3,17 @@ import boto3
 glue = boto3.client('glue', region_name='us-east-1')
 
 # Parameters
-database_name = 'test'
+database_name = 'test123'
 table_name = 'test-iceberg-table'
+s3_location = 's3://glue-iceberg-s3-test-poc/data/'
+
+glue.create_database(
+    DatabaseInput={
+        'Name': database_name,
+        'Description': 'Iceberg demo database created via Boto3',
+    }
+)
+print(f"Database '{database_name}' created.")
 
 # Create table request
 response = glue.create_table(
@@ -23,7 +32,7 @@ response = glue.create_table(
                 {'Name': 'name', 'Type': 'string'},
                 {'Name': 'event_time', 'Type': 'timestamp'}
             ],
-            'Location': 's3://glue-iceberg-s3-test-poc/data/',
+            'Location': s3_location,
             'InputFormat': 'org.apache.iceberg.mr.hive.HiveIcebergInputFormat',
             'OutputFormat': 'org.apache.iceberg.mr.hive.HiveIcebergOutputFormat',
             'SerdeInfo': {
